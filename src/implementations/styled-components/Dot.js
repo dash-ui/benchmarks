5c6aa6cb88d687565c81dfcdb2e6bbddb18e83f3
Jared Lunde <jared.lunde@gmail.com>
2020-08-07 23:00:56 -0600

    fix: fix styled-components dynamic styles case

--- src/implementations/styled-components/Dot.js
+++ src/implementations/styled-components/Dot.js
@@ -1,24 +1,21 @@
-import styled from 'styled-components';
-import View from './View';
+import styled from "styled-components";
+import View from "./View";
 
-const Dot = styled(View).attrs({
-  style: props => ({
-    marginLeft: `${props.x}px`,
-    marginTop: `${props.y}px`,
-    borderRightWidth: `${props.size / 2}px`,
-    borderBottomWidth: `${props.size / 2}px`,
-    borderLeftWidth: `${props.size / 2}px`,
-    borderBottomColor: `${props.color}`
-  })
-})`
-  position: absolute;
-  cursor: pointer;
-  width: 0;
-  height: 0;
-  border-color: transparent;
-  border-style: solid;
-  border-top-width: 0;
-  transform: translate(50%, 50%);
-`;
+const Dot = styled(View)((props) => ({
+  position: "absolute",
+  cursor: "pointer",
+  width: 0,
+  height: 0,
+  borderColor: "transparent",
+  borderStyle: "solid",
+  borderTopWidth: 0,
+  transform: "translate(50%, 50%)",
+  marginLeft: `${props.x}px`,
+  marginTop: `${props.y}px`,
+  borderRightWidth: `${props.size / 2}px`,
+  borderBottomWidth: `${props.size / 2}px`,
+  borderLeftWidth: `${props.size / 2}px`,
+  borderBottomColor: `${props.color}`,
+}));
 
 export default Dot;
